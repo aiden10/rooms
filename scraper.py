0916a626ba3609ccec862806106f6751eda3a736
aiden10 <aiden.ten30@gmail.com>
2024-10-10 21:04:25 -0400

    sort filter results alphabetically

--- scraper.py
+++ scraper.py
@@ -1,54 +1,7 @@
 """
-Buttons on the top of the page to filter and view the rooms which are currently empty and those which are empty for the rest of the day
-
-Finding currently empty rooms:
-<div id="currently-empty-rooms"></div>
-    var currentDate = new Date();
-    var currentTime = new Date(currentDate.getTime());
-    var currentlyEmptyRooms = [];
-    for (const roomDiv of rooms-container){ // doing it like this so that I can access the divs
-        let name = roomDiv.roomName;
-        let roomSchedule = rooms[name]; // rooms JSON file might not be accessible and I don't want to pass it around so this might not work
-        let valid = isValidTime(roomSchedule, currentTime.hour, currentTime.minute, currentTime.day, currentTime.suffix);
-        currentlyEmptyRooms.push(roomDiv);
-    }
-    rooms-container.style.display = "none"; // hide the original room list
-    
-    var emptyRoomsDiv = document.getElementById("currently-empty-rooms");
-    for (const room in currentlyEmptyRooms){
-        emptyRoomsDiv.appendChild(room);
-    }
-
-Finding rooms which are empty for the rest of the day:
-<div id="empty-rooms"></div>
-    var currentDate = new Date();
-    var currentTime = new Date(currentDate.getTime());
-    var emptyRooms = [];
-    var timeCopy = currentTime;
-    for (const roomDiv of rooms-container){ // doing it like this so that I can access the divs
-        let name = roomDiv.roomName;
-        var roomSchedule = rooms[name];
-        var roomValid = true;
-        while (timeCopy.day === currentTime.day){
-            let valid = isValidTime(roomSchedule, timeCopy.hour, timeCopy.minute, timeCopy.day, timeCopy.suffix);
-            if (!valid){
-                roomValid = false;
-                break;
-            }
-            timeCopy.minute += 10; 
-        }
-        if (roomValid){
-            emptyRooms.push(roomDiv);
-        }
-        timeCopy = currentTime;
-    }
-    rooms-container.style.display = "none"; // hide the original room list
-    
-    var emptyRoomsDiv = document.getElementById("empty-rooms");
-    for (const room in emptyRooms){
-        emptyRoomsDiv.appendChild(room);
-    }
-
+Get winter courses
+Add a search bar
+Sort alphabetically
 """
 import requests
 import json
